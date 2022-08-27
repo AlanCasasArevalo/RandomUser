9674788e3004b979a0a4780ffe993c435de24cbd
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-08-27 14:18:46 +0200

    api key

--- RandomUser/Commons/Constants.swift
+++ RandomUser/Commons/Constants.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct Constants {
     struct API {
+        static let apiKey = "YOUR_API_KEY"
         static var baseURL = URL(string: "https://randomuser.me/api")!
     }
 }
