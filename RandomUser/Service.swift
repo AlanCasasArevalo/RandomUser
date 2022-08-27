1013e38e795c64bc601414b7e394868dfa20439c
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-08-27 13:13:11 +0200

    Service to get user avatar was added

--- RandomUser/Service.swift
+++ RandomUser/Service.swift
@@ -17,4 +17,19 @@ struct Service {
             }
         }.resume()
     }
+    
+    public func getUserAvatar(from url: URL, completion:@escaping (Data?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                return completion(.none)
+            } else {
+                if let response = response as? HTTPURLResponse, let data = data {
+                    if response.statusCode > 199 && response.statusCode < 299 {
+                        completion(data)
+                    }
+                }
+                completion(.none)
+            }
+        }.resume()
+    }
 }
