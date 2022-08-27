5ea69bf3f05d1f706f24e028e1507b0345cf66eb
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-08-27 12:23:27 +0200

    Get users was makes as public method

--- RandomUser/Service.swift
+++ RandomUser/Service.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct Service {
 
-    func getUsers(from url: URL, completion:@escaping ([User]?) -> ()) {
+    public func getUsers(from url: URL, completion:@escaping ([User]?) -> ()) {
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 return completion(.none)
